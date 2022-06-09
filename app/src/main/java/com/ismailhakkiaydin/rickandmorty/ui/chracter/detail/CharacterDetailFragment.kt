15c409e33e4eddeddd2be0f646076473319aa68d
ihaydinn <ihaydin7@gmail.com>
2022-06-09 20:38:26 +0300

    Add di

--- app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/detail/CharacterDetailFragment.kt
+++ app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/detail/CharacterDetailFragment.kt
@@ -2,8 +2,10 @@ package com.ismailhakkiaydin.rickandmorty.ui.chracter.detail
 
 import com.ismailhakkiaydin.rickandmorty.base.BaseFragment
 import com.ismailhakkiaydin.rickandmorty.databinding.FragmentCharacterDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class CharacterDetailFragment :
     BaseFragment<FragmentCharacterDetailBinding, CharacterDetailViewModel>(
         FragmentCharacterDetailBinding::inflate
