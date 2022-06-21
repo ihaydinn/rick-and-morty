429e7a84c2684b7fd5d202f334d1df05fbae4fc2
ihaydinn <ihaydin7@gmail.com>
2022-06-21 22:59:39 +0300

    Add listing characters

--- app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/CharacterFragment.kt
+++ app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/CharacterFragment.kt
@@ -5,15 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.ismailhakkiaydin.rickandmorty.R
 import com.ismailhakkiaydin.rickandmorty.base.BaseFragment
 import com.ismailhakkiaydin.rickandmorty.databinding.ActivityMainBinding
 import com.ismailhakkiaydin.rickandmorty.databinding.FragmentCharacterBinding
+import com.ismailhakkiaydin.rickandmorty.ui.chracter.adapter.CharacterAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CharacterFragment :
     BaseFragment<FragmentCharacterBinding, CharacterViewModel>(FragmentCharacterBinding::inflate) {
 
+    lateinit var adapter: CharacterAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,6 +27,19 @@ class CharacterFragment :
 
 
         viewModel.getCharacters("1")
+
+        binding?.recyclerViewCharacters?.adapter = CharacterAdapter{
+            findNavController().navigate(R.id.action_characterFragment_to_characterDetailFragment)
+        }.apply {
+            adapter = this
+        }
+
+        viewModel.data.observe(viewLifecycleOwner){
+            it?.let {
+                adapter.submitList(it)
+            }
+        }
+
         return binding?.root
 
     }
--- app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/CharacterViewModel.kt
+++ app/src/main/java/com/ismailhakkiaydin/rickandmorty/ui/chracter/CharacterViewModel.kt
@@ -1,7 +1,10 @@
 package com.ismailhakkiaydin.rickandmorty.ui.chracter
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.ismailhakkiaydin.rickandmorty.base.BaseViewModel
+import com.ismailhakkiaydin.rickandmorty.data.entity.Character
 import com.ismailhakkiaydin.rickandmorty.data.mapper.toDomainModel
 import com.ismailhakkiaydin.rickandmorty.data.repository.CharacterRepository
 import com.ismailhakkiaydin.rickandmorty.util.Resource
@@ -15,17 +18,21 @@ class CharacterViewModel @Inject constructor(
     private val repository: CharacterRepository
 ) : BaseViewModel() {
 
+    private val _data = MutableLiveData<List<Character>>()
+    val data: LiveData<List<Character>> get() = _data
 
-    fun getCharacters(page: String){
+    fun getCharacters(page: String) {
         viewModelScope.launch {
-            when(val result = repository.getCharacters(page = page)){
+            when (val result = repository.getCharacters(page = page)) {
                 is Resource.Success -> {
-                    result?.data.let { character ->
+                    result.data.let { character ->
+                        val list = arrayListOf<Character>()
                         character?.results?.forEach {
                             it?.let {
-                                it.toDomainModel()
+                                list.add(it.toDomainModel())
                             }
                         }
+                        _data.value = list
                     }
                 }
                 is Resource.Error -> {
@@ -33,6 +40,7 @@ class CharacterViewModel @Inject constructor(
                         val a = it
                     }
                 }
+                else -> {}
             }
 
         }
